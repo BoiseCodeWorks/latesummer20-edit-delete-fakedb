34c108b4fad3968cec228bc6992ae09d9c6ccc0a
Tim Dohmen <dohmentim@yahoo.com>
2020-08-27 18:07:27 -0600

    fireside complete

--- Controllers/CatsController.cs
+++ Controllers/CatsController.cs
@@ -9,58 +9,84 @@ using Microsoft.Extensions.Logging;
 
 namespace catsAPI.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class CatsController : ControllerBase
+  [ApiController]
+  [Route("api/[controller]")]
+  public class CatsController : ControllerBase
+  {
+    private readonly CatsService _catService;
+    public CatsController(CatsService catsService)
     {
-        private readonly CatsService _catService;
-        public CatsController(CatsService catsService)
-        {
-            _catService = catsService;
-        }
-
-        [HttpGet]
-        public ActionResult<IEnumerable<Cat>> Get()
-        {
-            try
-            {
-                IEnumerable<Cat> cats = _catService.GetCats();
-                return Ok(cats);
-            }
-            catch (Exception err)
-            {
-                return BadRequest(err.Message);
-            }
-        }
-
-        [HttpGet("{id}")]
-        public ActionResult<Cat> GetById(string id)
-        {
-            try
-            {
-                Cat cat = _catService.GetById(id);
-                return Ok(cat);
-            }
-            catch (Exception err)
-            {
-
-                return BadRequest(err.Message);
-            }
-        }
-
-        [HttpPost]
-        public ActionResult<Cat> Create([FromBody] Cat newCat)
-        {
-            try
-            {
-                return Ok(_catService.Create(newCat));
-            }
-            catch (System.Exception err)
-            {
-
-                return BadRequest(err.Message);
-            }
-        }
-
+      _catService = catsService;
     }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Cat>> Get()
+    {
+      try
+      {
+        IEnumerable<Cat> cats = _catService.GetCats();
+        return Ok(cats);
+      }
+      catch (Exception err)
+      {
+        return BadRequest(err.Message);
+      }
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Cat> GetById(string id)
+    {
+      try
+      {
+        Cat cat = _catService.GetById(id);
+        return Ok(cat);
+      }
+      catch (Exception err)
+      {
+
+        return BadRequest(err.Message);
+      }
+    }
+
+    [HttpPost]
+    public ActionResult<Cat> Create([FromBody] Cat newCat)
+    {
+      try
+      {
+        return Ok(_catService.Create(newCat));
+      }
+      catch (System.Exception err)
+      {
+
+        return BadRequest(err.Message);
+      }
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult<Cat> Edit([FromBody] Cat editCat, string id)
+    {
+      try
+      {
+        editCat.Id = id;
+        return Ok(_catService.Edit(editCat));
+      }
+      catch (Exception err)
+      {
+        return BadRequest(err.Message);
+      }
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult<String> Delete(string id)
+    {
+      try
+      {
+        return Ok(_catService.Delete(id));
+      }
+      catch (System.Exception err)
+      {
+        return BadRequest(err.Message);
+      }
+    }
+  }
 }
--- Services/CatsService.cs
+++ Services/CatsService.cs
@@ -7,27 +7,50 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace catsAPI.Services
 {
-    public class CatsService
+  public class CatsService
+  {
+    public IEnumerable<Cat> GetCats()
     {
-        public IEnumerable<Cat> GetCats()
-        {
-            return FakeDB.Cats;
-        }
-
-        public Cat GetById(string id)
-        {
-            Cat foundCat = FakeDB.Cats.Find(cat => cat.Id == id);
-            if (foundCat != null)
-            {
-                return foundCat;
-            }
-            throw new Exception("Opps no cat by that Id");
-        }
-
-        internal object Create(Cat newCat)
-        {
-            FakeDB.Cats.Add(newCat);
-            return newCat;
-        }
+      return FakeDB.Cats;
     }
+
+    public Cat GetById(string id)
+    {
+      Cat foundCat = FakeDB.Cats.Find(cat => cat.Id == id);
+      if (foundCat != null)
+      {
+        return foundCat;
+      }
+      throw new Exception("Opps no cat by that Id");
+    }
+
+    internal object Create(Cat newCat)
+    {
+      FakeDB.Cats.Add(newCat);
+      return newCat;
+    }
+
+    internal Cat Edit(Cat editCat)
+    {
+      var current = FakeDB.Cats.Find(c => c.Id == editCat.Id);
+      if (current == null)
+      {
+        throw new Exception("Invalid Id");
+      }
+      FakeDB.Cats.Remove(current);
+      FakeDB.Cats.Add(editCat);
+      return editCat;
+    }
+
+    internal String Delete(string id)
+    {
+      var current = FakeDB.Cats.Find(c => c.Id == id);
+      if (current == null)
+      {
+        throw new Exception("Invalid Id");
+      }
+      FakeDB.Cats.Remove(current);
+      return current.Name + " ran away";
+    }
+  }
 }
